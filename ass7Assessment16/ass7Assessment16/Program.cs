1622c9721f957f139b2f2f476a7c03c772f6a67e
Bhekani Masinga <bhekani.masinga@nagarro.com>
2023-07-08 19:55:52 +0200

    first commit

---
+++ ass7Assessment16/ass7Assessment16/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public class FileAnalyzer
+{
+    // Function to get all files in a directory
+    public static string[] GetFilesInDirectory(string directory)
+    {
+        return Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+    }
+
+    // Function to count the number of text files (*.txt)
+    public static int CountTextFiles(string directory)
+    {
+        string[] files = GetFilesInDirectory(directory);
+        int textFileCount = files.Count(file => Path.GetExtension(file) == ".txt");
+        return textFileCount;
+    }
+
+    // Function to count the number of files per extension type
+    public static int[] CountFilesPerExtension(string directory)
+    {
+        string[] files = GetFilesInDirectory(directory);
+        var fileCounts = files.GroupBy(file => Path.GetExtension(file))
+                              .Select(group => new { Extension = group.Key, Count = group.Count() })
+                              .ToArray();
+        int[] counts = new int[fileCounts.Length];
+        for (int i = 0; i < fileCounts.Length; i++)
+        {
+            counts[i] = fileCounts[i].Count;
+        }
+        return counts;
+    }
+
+    // Function to get the top 5 largest files
+    public static (string, long)[] GetTop5LargestFiles(string directory)
+    {
+        string[] files = GetFilesInDirectory(directory);
+        var fileSizes = files.Select(file => new { Path = file, Size = new FileInfo(file).Length })
+                             .OrderByDescending(file => file.Size)
+                             .Take(5)
+                             .Select(file => (file.Path, file.Size))
+                             .ToArray();
+        return fileSizes;
+    }
+
+    // Function to get the file with maximum length
+    public static string GetFileWithMaxLength(string directory)
+    {
+        string[] files = GetFilesInDirectory(directory);
+        string maxFile = "";
+        long maxLength = 0;
+        foreach (string file in files)
+        {
+            long length = new FileInfo(file).Length;
+            if (length > maxLength)
+            {
+                maxLength = length;
+                maxFile = file;
+            }
+        }
+        return maxFile;
+    }
+
+    // Example usage
+    public static void Main(string[] args)
+    {
+        string directoryPath = "C:\\Users\\bhekanimasinga\\3D Objects";
+        int numTextFiles = CountTextFiles(directoryPath);
+        int[] fileCountPerExtension = CountFilesPerExtension(directoryPath);
+        var top5LargestFiles = GetTop5LargestFiles(directoryPath);
+        string fileWithMaxLength = GetFileWithMaxLength(directoryPath);
+
+        Console.WriteLine("Number of text files (*.txt): " + numTextFiles);
+        Console.WriteLine("Number of files per extension type: ");
+        string[] extensions = Directory.GetDirectories(directoryPath);
+        for (int i = 0; i < extensions.Length; i++)
+        {
+            Console.WriteLine(extensions[i] + ": " + fileCountPerExtension[i]);
+        }
+        Console.WriteLine("Top 5 largest files: ");
+        foreach (var file in top5LargestFiles)
+        {
+            Console.WriteLine($"File: {file.Item1}, Size: {file.Item2}");
+        }
+        Console.WriteLine("File with maximum length: " + fileWithMaxLength);
+    }
+}
---
+++ ass7Assessment17/ass7Assessment17/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+public class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
+
+public class Game
+{
+    private static int numGamesPlayed = 0;
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            while (true)
+            {
+                Step3();
+                int userInput = GetUserInput();
+                Step4(userInput);
+                numGamesPlayed++;
+
+                if (numGamesPlayed >= 5)
+                {
+                    Console.WriteLine("You have played this game for 5 times.");
+                    break;
+                }
+            }
+        }
+        catch (CustomException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            Main(args); // Restart the game
+        }
+    }
+
+    private static void Step3()
+    {
+        Console.WriteLine("Enter any number from 1-5:");
+    }
+
+    private static int GetUserInput()
+    {
+        string input = Console.ReadLine();
+        int userInput;
+        bool isValid = int.TryParse(input, out userInput);
+
+        if (!isValid || userInput < 1 || userInput > 5)
+        {
+            throw new CustomException("Invalid input. Please enter a number from 1-5.");
+        }
+
+        return userInput;
+    }
+
+    private static void Step4(int userInput)
+    {
+        switch (userInput)
+        {
+            case 1:
+                Console.WriteLine("Enter even number.");
+                break;
+            case 2:
+                Console.WriteLine("Enter odd number.");
+                break;
+            case 3:
+                Console.WriteLine("Enter a prime number.");
+                break;
+            case 4:
+                Console.WriteLine("Enter a negative number.");
+                break;
+            case 5:
+                Console.WriteLine("Enter zero.");
+                break;
+            default:
+                throw new CustomException("Invalid input. Please enter a number from 1-5.");
+        }
+
+        int number = GetNumberInput();
+
+        switch (userInput)
+        {
+            case 1:
+                if (number % 2 != 0)
+                {
+                    throw new CustomException("Error: Not an even number.");
+                }
+                break;
+            case 2:
+                if (number % 2 == 0)
+                {
+                    throw new CustomException("Error: Not an odd number.");
+                }
+                break;
+            case 3:
+                if (!IsPrime(number))
+                {
+                    throw new CustomException("Error: Not a prime number.");
+                }
+                break;
+            case 4:
+                if (number >= 0)
+                {
+                    throw new CustomException("Error: Not a negative number.");
+                }
+                break;
+            case 5:
+                if (number != 0)
+                {
+                    throw new CustomException("Error: Not zero.");
+                }
+                break;
+        }
+
+        Console.WriteLine("Success!");
+    }
+
+    private static int GetNumberInput()
+    {
+        string input = Console.ReadLine();
+        int number;
+        bool isValid = int.TryParse(input, out number);
+
+        if (!isValid)
+        {
+            throw new CustomException("Invalid number input.");
+        }
+
+        return number;
+    }
+
+    private static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
